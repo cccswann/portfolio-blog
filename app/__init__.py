e0d5863f6278dbf902c3333999cb7a74166285d9
Ciara Swann <cccswann@gmail.com>
2021-07-09 13:22:50 -0700

    Update init file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 import os
 from flask import Flask, Response, render_template, send_from_directory
 from dotenv import load_dotenv
+from . import db
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
+db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def health():
-    return Response("HTTP 200 OK", mimetype='text/html'), 200
+    return Response("HTTP 200 OK", mimetype="text/html"), 200
